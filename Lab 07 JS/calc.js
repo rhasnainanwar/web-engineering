60865a98ba5c40df4fa68024a380872142303cdd
Raja Hasnain Anwar <rajahasnain570@gmail.com>
2018-03-29 12:52:50 +0500

    js calc

---
+++ Lab 07 JS/calc.js
@@ -0,0 +1,71 @@
+var tar = document.getElementById("bar");
+var m = null;
+// append to input expression
+function append(e) {
+	if(tar.innerHTML.substring(tar.innerHTML.length - 2) == ".0")
+		tar.innerHTML = tar.innerHTML.substring(0, tar.innerHTML.length - 1) + e.innerHTML;
+	else 
+		tar.innerHTML += e.innerHTML;
+}
+
+function clearInput() {
+	tar.innerHTML = "";
+	m = null;
+}
+
+// calculate the current input value
+function calc(){
+	if(tar.innerHTML != ""){
+		exp = tar.innerHTML
+		exp = exp.replace('x', '*');
+		return eval(exp);
+	}
+}
+
+// respond to equal command
+function getResult(){
+	if(tar.innerHTML != "")
+		tar.innerHTML = calc();
+}
+
+function invert() {
+	if(tar.innerHTML != "")
+		tar.innerHTML = -calc();
+}
+
+function sqroo(){
+	if(tar.innerHTML != "")
+		tar.innerHTML = Math.sqrt(calc());
+}
+
+function sq(){
+	if(tar.innerHTML != "")
+		tar.innerHTML = Math.pow(calc(), 2);
+}
+
+function reciprocal(){
+	if(tar.innerHTML != "")
+		tar.innerHTML = 1/calc();
+}
+
+function clmem(){
+	m = null;
+}
+
+function store(){
+	var val = calc();
+	if(tar.innerHTML == val){
+		m = val;
+		tar.innerHTML = "";
+	}
+}
+
+function restore(){
+	if(m != null)
+		tar.innerHTML += m;
+}
+
+function mplus(){
+	m += calc();
+	tar.innerHTML = "";
+}
